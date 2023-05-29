aa58e762b2e50561df8dd75d7c6f5f2084d2e29f
Nidjat <gurbanli.nidjat001@gmail.com>
2023-05-29 15:29:31 +0400

    added other hws

---
+++ Homeworks-main/FirstASPHW(Web App)/Pages/Index.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private Random rnd = new();
+
+        private readonly ILogger<IndexModel> _logger;
+
+        private int Position { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ViewData["DateTimeTotalDays"] = ShowDateTime();
+            ViewData["ASCIIChar"] = ShowChar();
+            ViewData["Position"] = Position;
+        }
+
+        public int ShowDateTime()
+        {
+            DateTime end = DateTime.Now;
+            DateTime start = new(DateTime.Now.Year, 1, 1);
+
+            return (int)(end - start).TotalDays;
+        }
+
+        public string ShowChar()
+        {
+            Position = rnd.Next(65, 91);
+
+            char character = (char)Position;
+            string text = character.ToString();
+
+            return text;
+        }
+    }
+}
---
+++ Homeworks-main/GetIP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+
+var url = @"https://2ip.ru";
+Console.WriteLine(await GetIP.GetExternalIP(url));
+
+public class GetIP
+{
+    public async static Task<string> GetExternalIP(string api)
+    {
+        HttpClient httpClient = new ();
+        HttpRequestMessage requestMessage = new (HttpMethod.Get, api);
+
+        requestMessage.Headers.Add("User-Agent", "User-Agent-Here");
+        HttpResponseMessage response = await httpClient.SendAsync(requestMessage);
+
+        //? - Соответствует нулю или одному экземпляру предшествующего шаблона; предшествующий шаблон является необязательным
+        //+ - Соответствует одному или более экземплярам предшествующего шаблона
+        //* - Соответствует нулю или более экземплярам предшествующего шаблона
+        //[0-9] - только цифры
+        //. - Любой символ, кроме перевода строки или другого разделителя Unicode-строки
+        //(...) - Группировка. Группирует элементы в единое целое, которое может использоваться с символами *, +, ?, | и т.п. Также запоминает символы, соответствующие этой группе для использования в последующих ссылках.
+
+        Match _match = Regex.Match(response.ToString(), "=([0-9]+.*?);");
+
+        return $"Текущий IP: {_match.ToString().Substring(1)}";
+    }
+}
+
